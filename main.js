024995bd7c3541f6870dd24b79e0c3eabfc14c37
Chino Miles <anazobajames@gmail.com>
2020-03-26 01:01:37 +0100

    main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+let name = "Anazoba James";
+let course = ["HTML", "CSS", "JavaScript", "UI/UX."];
+
+console.log("My name is:" + name + " and i'm taking these courses:" + course);
+ 
+for (let i = 1; i < 202; i++) {
+    if (i % 2===0) console.log(i);
+}
